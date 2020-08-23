f9c9fa63dff35085c18e639198da8388510c53fe
Mison <mison.caamwang@hotmail.com>
2020-08-24 00:29:20 +1000

    new

--- src/App.js
+++ src/App.js
@@ -6,8 +6,20 @@ import CountryPicker from './components/CountryPicker/CountryPicker';
 // importing css styles, .module.css files enable the css styles to have a local scope. 
 import styles from './App.module.css';
 
+import { fetchData } from './api/index';
+
 // Only class based componenet that we need. Class based component cannot use hooks. 
 class App extends React.Component {
+  // write additional js code here.
+  
+  state = {
+    data: {},
+  }
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log(data);
+  }
   render() {
     return (
       // JSX 
