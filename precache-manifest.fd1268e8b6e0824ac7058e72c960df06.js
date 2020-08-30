beadae883fdefd8e1e4fb419285a9a09062dd484
Mison <mison.caamwang@hotmail.com>
2020-08-30 19:34:46 +1000

    Updates

---
+++ precache-manifest.fd1268e8b6e0824ac7058e72c960df06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41182c32f3c83a5cdd70befbb9b839fe",
+    "url": "/covid-vis-react/index.html"
+  },
+  {
+    "revision": "48f7beb8dbfac03e099a",
+    "url": "/covid-vis-react/static/css/main.23c49c30.chunk.css"
+  },
+  {
+    "revision": "a62eabfabc61a5c891a0",
+    "url": "/covid-vis-react/static/js/2.467add31.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-vis-react/static/js/2.467add31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f7beb8dbfac03e099a",
+    "url": "/covid-vis-react/static/js/main.82158519.chunk.js"
+  },
+  {
+    "revision": "c90ec5e926a219fb4b45",
+    "url": "/covid-vis-react/static/js/runtime-main.8b49404d.js"
+  }
+]);
